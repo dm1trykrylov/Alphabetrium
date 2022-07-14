e63192c25590b57deeecd51ceacec6b908b03d41
Dmitry Krylov <231014d@gmail.com>
2022-07-14 19:06:11 +0400

    create main page with search bar

--- app/src/main/java/com/thegarage/alphabetrium/MainActivity.kt
+++ app/src/main/java/com/thegarage/alphabetrium/MainActivity.kt
@@ -2,10 +2,22 @@ package com.thegarage.alphabetrium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
+    // creating array list for listview
+    lateinit var programmingLanguagesList: ArrayList<String>
+
+    // creating variable for searchview
+    lateinit var searchView: SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchView = findViewById(R.id.searchView)
     }
+
 }
